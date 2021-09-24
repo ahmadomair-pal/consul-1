arunguleria/consul

Use Active Storage to handle cached attachments
This fixes a few issues we've had for years.

First, when attaching an image and then sending a form with validation
errors, the image preview would not be rendered when the form was
displayed once again. Now it's rendered as expected.

Second, when attaching an image, removing it, and attaching a new
one, browsers were displaying the image preview of the first one. That's
because Paperclip generated the same URL from both files (as they both
had the same hash data and prefix). Browsers usually cache images and
render the cached image when getting the same URL.

Since now we're storing each image in a different Blob, the images have
different URLs and so the preview of the second one is correctly
displayed.

Finally, when users downloaded a document, they were getting files with
a very long hexadecimal hash as filename. Now they get the original
filename.

app/models/concerns/attachable.rb
@@ -9,14 +9,10 @@ module Attachable
     # Paperclip do not allow to use Procs on valiations definition
     do_not_validate_attachment_file_type :attachment
     validate :attachment_presence
-    validate :validate_attachment_content_type,         if: -> { attachment.present? }
-    validate :validate_attachment_size,                 if: -> { attachment.present? }
+    validate :validate_attachment_content_type,         if: -> { storage_attachment.attached? }
+    validate :validate_attachment_size,                 if: -> { storage_attachment.attached? }
 
-    before_save :set_attachment_from_cached_attachment, if: -> { cached_attachment.present? }
-
-    Paperclip.interpolates :prefix do |attachment, style|
-      attachment.instance.prefix(attachment, style)
-    end
+    before_validation :set_attachment_from_cached_attachment, if: -> { cached_attachment.present? }
   end
 
   def url(style = nil)
@@ -38,27 +34,37 @@ module Attachable
   end
 
   def set_cached_attachment_from_attachment
-    self.cached_attachment = if Paperclip::Attachment.default_options[:storage] == :filesystem
-                               attachment.path
-                             else
-                               attachment.url
-                             end
+    # TODO: remote storage?
+    self.cached_attachment = storage_attachment.signed_id
   end
 
   def set_attachment_from_cached_attachment
+    # TODO: remote storage?
+    self.storage_attachment = cached_attachment
+
     if Paperclip::Attachment.default_options[:storage] == :filesystem
-      File.open(cached_attachment) { |file| self.attachment = file }
+      File.open(file_path) do |file|
+        self.paperclip_attachment = file
+      end
     else
-      self.attachment = URI.parse(cached_attachment)
+      self.paperclip_attachment = URI.parse(cached_attachment)
     end
   end
 
-  def prefix(attachment, _style)
-    if !attachment.instance.persisted?
-      "cached_attachments/user/#{attachment.instance.user_id}"
+  def attachment_content_type
+    storage_attachment.blob.content_type if storage_attachment.attached?
+  end
+
+  def attachment_file_size
+    if storage_attachment.attached?
+      storage_attachment.blob.byte_size
     else
-      ":attachment/:id_partition"
+      0
     end
+  end
+
+  def file_path
+    ActiveStorage::Blob.service.path_for(storage_attachment.blob.key)
   end
 
   private
@@ -81,7 +87,7 @@ module Attachable
     end
 
     def attachment_presence
-      if attachment.blank? && cached_attachment.blank?
+      unless storage_attachment.attached?
         errors.add(:attachment, I18n.t("errors.messages.blank"))
       end
     end

ahmadomair-pal/consul-1

app/models/concerns/attachable.rb
@@ -9,14 +9,10 @@ module Attachable
     # Paperclip do not allow to use Procs on valiations definition
     do_not_validate_attachment_file_type :attachment
     validate :attachment_presence
-    validate :validate_attachment_content_type,         if: -> { attachment.present? }
-    validate :validate_attachment_size,                 if: -> { attachment.present? }
+    validate :validate_attachment_content_type,         if: -> { storage_attachment.attached? }
+    validate :validate_attachment_size,                 if: -> { storage_attachment.attached? }
 
-    before_save :set_attachment_from_cached_attachment, if: -> { cached_attachment.present? }
-
-    Paperclip.interpolates :prefix do |attachment, style|
-      attachment.instance.prefix(attachment, style)
-    end
+    before_validation :set_attachment_from_cached_attachment, if: -> { cached_attachment.present? }
   end
 
   def association_class
@@ -26,27 +22,35 @@ module Attachable
   end
 
   def set_cached_attachment_from_attachment
-    self.cached_attachment = if Paperclip::Attachment.default_options[:storage] == :filesystem
-                               attachment.path
-                             else
-                               attachment.url
-                             end
+    self.cached_attachment = storage_attachment.signed_id
   end
 
   def set_attachment_from_cached_attachment
+    self.storage_attachment = cached_attachment
+
     if Paperclip::Attachment.default_options[:storage] == :filesystem
-      File.open(cached_attachment) { |file| self.attachment = file }
+      File.open(file_path) do |file|
+        self.paperclip_attachment = file
+      end
     else
-      self.attachment = URI.open(cached_attachment)
+      self.paperclip_attachment = URI.open(cached_attachment)
     end
   end
 
-  def prefix(attachment, _style)
-    if attachment.instance.persisted?
-      ":attachment/:id_partition"
+  def attachment_content_type
+    storage_attachment.blob.content_type if storage_attachment.attached?
+  end
+
+  def attachment_file_size
+    if storage_attachment.attached?
+      storage_attachment.blob.byte_size
     else
-      "cached_attachments/user/#{attachment.instance.user_id}"
+      0
     end
+  end
+
+  def file_path
+    ActiveStorage::Blob.service.path_for(storage_attachment.blob.key)
   end
 
   private
@@ -69,7 +73,7 @@ module Attachable
     end
 
     def attachment_presence
-      if attachment.blank? && cached_attachment.blank?
+      unless storage_attachment.attached?
         errors.add(:attachment, I18n.t("errors.messages.blank"))
       end
     end